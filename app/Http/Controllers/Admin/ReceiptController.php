3da0abf03ac4804509c1f2502ef2967b159f721b
thanhkudo <thanhkudo1o11998@gmail.com>
2025-05-08 15:03:28 +0700

    Kudo - Update view  receipt

--- app/Http/Controllers/Admin/ReceiptController.php
+++ app/Http/Controllers/Admin/ReceiptController.php
@@ -117,9 +117,14 @@ class ReceiptController extends Controller
         DB::beginTransaction();
         try {
             $receipt = Receipt::find($id);
+            $json_params = json_decode(json_encode($receipt->json_params), true);
+            // Cập nhật lại thông tin
+            $json_params['payment_deadline'] = $request->input('payment_deadline');
             $receipt->status = Consts::STATUS_RECEIPT['paid'];
-            $receipt->total_paid = $receipt->total_amount;
+            $receipt->total_paid = $request->input('total_paid');
+            $receipt->total_due = $receipt->total_final + $receipt->prev_balance - $request->input('total_paid');
             $receipt->cashier_id = $admin->id;
+            $receipt->json_params = $json_params;
 
             // Cập nhật trạng thái của prev_receipt nếu tồn tại
             if ($receipt->prev_receipt) {
@@ -143,8 +148,97 @@ class ReceiptController extends Controller
         $json_params = json_decode(json_encode($receipt->json_params), true);
         $json_params['explanation'] = $explanation;
         $receipt->prev_balance = $prev_balance;
+
         $receipt->json_params = $json_params;
         $receipt->save();
         return $this->sendResponse('success', 'Cập nhật thành công!');
+    }
+
+    public function print(Request $request, $id)
+    {
+        $receipt = Receipt::find($id);
+        $this->responseData['detail'] = $receipt;
+
+        $receiptDetails = $receipt->receiptDetail->load('services_receipt'); // Nạp quan hệ service
+
+        // Gộp các receiptDetail theo service_id và tính tổng amount
+        $groupedDetails = $receiptDetails->groupBy('service_id')->map(function ($details) {
+            return [
+                'service' => optional($details->first()->services_receipt),
+                'service_type' => optional($details->first()->services_receipt)->service_type, // Lấy service_type từ quan hệ service
+                'total_amount' => $details->sum('amount'), // Tính tổng amount
+                'total_discount_amount' => $details->sum('discount_amount'), // Tính tổng discount_amount
+                'min_month' => $details->min('month'), // Tháng áp dụng nhỏ nhất
+                'max_month' => $details->max('month'), // Tháng áp dụng lớn nhất
+            ];
+        });
+
+        // Gộp theo service_type và tính tổng total_amount
+        $groupedByServiceType = $groupedDetails->groupBy('service_type')->map(function ($details, $serviceType) {
+            return [
+                'service_type' => $serviceType, // Loại dịch vụ
+                'total_amount' => $details->sum('total_amount'), // Tổng tiền của loại
+                'total_discount_amount' => $details->sum('total_discount_amount'), // Tổng tiền của loại
+                'services' => $details, // Chi tiết từng nhóm dịch vụ
+            ];
+        });
+
+        $monthlyDetails = $groupedByServiceType->get('monthly', collect()); // Dịch vụ loại monthly
+        $yearlyDetails = $groupedByServiceType->get('yearly', collect()); // Dịch vụ loại monthly
+        // Lấy các loại còn lại ngoài monthly và yearly
+        $otherDetails = $groupedByServiceType
+            ->reject(function ($item, $key) {
+                return in_array($key, ['monthly', 'yearly']); // Loại trừ monthly và yearly
+            })
+            ->reduce(function ($carry, $item) {
+                // Gộp các nhóm khác lại
+                $carry['service_type'] = 'other'; // Đặt tên nhóm
+                $carry['total_final_amount'] = ($carry['total_final_amount'] ?? 0) + $item['total_final_amount']; // Tính tổng
+                $carry['services'] = ($carry['services'] ?? collect())->merge($item['services']); // Gộp chi tiết
+                return $carry;
+            }, []);
+
+        dd($otherDetails);
+        // Lấy thông tin giảm trừ
+        $groupedBytServiceDiscoun = $receiptDetails
+            ->filter(function ($detail) {
+                return $detail->discount_amount > 0; // Lọc các dịch vụ có discount_amount > 0
+            })
+            ->groupBy('service_id') // Nhóm theo service_id
+            ->map(function ($details, $serviceId) {
+                return [
+                    'service_id' => $serviceId, // ID của dịch vụ
+                    'service' => optional($details->first()->services_receipt), // Thông tin dịch vụ
+                    'total_discount_amount' => $details->sum('discount_amount'), // Tổng discount_amount của nhóm
+                ];
+            })->first();
+
+
+        // Lọc từng nhóm
+        $this->responseData['groupedByServiceType'] = $groupedByServiceType;
+        $this->responseData['groupedBytServiceDiscoun'] = $groupedBytServiceDiscoun;
+        return $this->responseView($this->viewPart . '.print');
+    }
+
+    // Chuyển số sang số la mã
+    function intToRoman($num)
+    {
+        // Mảng chứa giá trị La Mã tương ứng với số nguyên
+        $map = [
+            'X'  => 10,
+            'IX' => 9,
+            'V'  => 5,
+            'IV' => 4,
+            'I'  => 1,
+        ];
+        $result = '';
+        foreach ($map as $roman => $value) {
+            // Xác định số lần giá trị La Mã có thể lặp lại
+            while ($num >= $value) {
+                $result .= $roman; // Thêm ký tự La Mã vào kết quả
+                $num -= $value;    // Giảm số nguyên đi giá trị tương ứng
+            }
+        }
+        return $result;
     }
 }
--- storage/framework/views/efe55bb8c787e877c5517646125c41f40ead4e22.php
+++ storage/framework/views/efe55bb8c787e877c5517646125c41f40ead4e22.php
@@ -2,7 +2,7 @@
     <ul class="nav nav-tabs">
         <li class="active">
             <a href="#tab_thongtin" data-toggle="tab">
-                <h5 class="fw-bold">Thông tin hóa đơn</h5>
+                <h5 class="fw-bold">Thông tin TBP</h5>
             </a>
         </li>
         <li class="">
@@ -18,7 +18,7 @@
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <div class="form-group">
                             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                                <label class="control-label"><strong><?php echo app('translator')->get('Mã hóa đơn'); ?></strong></label>
+                                <label class="control-label"><strong><?php echo app('translator')->get('Mã TBP'); ?></strong></label>
                             </div>
                             <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
                                 <p><?php echo e($detail->receipt_code ?? ''); ?></p>
@@ -28,7 +28,7 @@
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <div class="form-group">
                             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-                                <label class="control-label"><strong><?php echo app('translator')->get('Tên hóa đơn'); ?></strong></label>
+                                <label class="control-label"><strong><?php echo app('translator')->get('Tên TBP'); ?></strong></label>
                             </div>
                             <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
                                 <p><?php echo e($detail->receipt_name ?? ''); ?></p>
@@ -215,9 +215,9 @@
                                             <th><?php echo app('translator')->get('Đơn giá'); ?></th>
                                             <th><?php echo app('translator')->get('Thành tiền'); ?></th>
                                             <th><?php echo app('translator')->get('Giảm trừ'); ?></th>
-                                            <th><?php echo app('translator')->get('Truy thu (+) / Hoàn trả (-)'); ?></th>
+                                            
                                             <th><?php echo app('translator')->get('Tổng tiền cuối cùng'); ?></th>
-                                            <th><?php echo app('translator')->get('Trạng thái'); ?></th>
+                                            
                                             <th style="width:250px"><?php echo app('translator')->get('Ghi chú'); ?></th>
                                         </tr>
                                     </thead>
@@ -233,9 +233,9 @@
                                                     <td><?php echo e(number_format($item->unit_price, 0, ',', '.')); ?></td>
                                                     <td><?php echo e(number_format($item->amount, 0, ',', '.')); ?></td>
                                                     <td><?php echo e(number_format($item->discount_amount, 0, ',', '.')); ?></td>
-                                                    <td><?php echo e(number_format($item->adjustment_amount, 0, ',', '.')); ?></td>
+                                                    
                                                     <td><?php echo e(number_format($item->final_amount, 0, ',', '.')); ?></td>
-                                                    <td><?php echo e(__($item->status)); ?></td>
+                                                    
                                                     <td><?php echo ($item->note); ?></td>
                                                 </tr>
                                             <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
